0a0c15d246fba1d8f42cd60a4f4d9d0ee0d194c6
Uzair Riaz <uriaz@gapp.co>
2024-01-31 14:44:26 +0500

    modal closed upon request success/error

--- src/components/service-card/FeedbackModal.jsx
+++ src/components/service-card/FeedbackModal.jsx
@@ -120,7 +120,7 @@ function FeedbackForm( {user, meetingId, attendeeEmail, onClose} ) {
                 <button
                 className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm p-2 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                 type="button"
-                onClick={() => saveFeedback({given_by: user.email, given_to: attendeeEmail, meetingId, feedback})}
+                onClick={() => saveFeedback({given_by: user.email, given_to: attendeeEmail, meetingId, feedback}).then(onClose)}
                 >
                 Submit
                 </button>
