741816a591fdbfbe463e057cae4778de9a053161
Uzair Riaz <uriaz@gapp.co>
2024-03-21 02:01:30 +0500

    fix issues

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
     return(
         <div className={'mx-auto max-w-7xl sm:px-6 lg:px-8 border-solid border-b border-gray-50 border-opacity-15 py-2'}>
             <div className={'footer py-3 text-left text-gray-400'}>
-                <p>© 2021 Tailwind Labs Inc. All rights reserved.</p>
+                <p>© 2024 Receptive AI. All rights reserved.</p>
             </div>
         </div>
 
